4689eae1b84f66d82735f45f33ebf60518ef0986
ChaseHaluptzok <c.haluptzok7@gmail.com>
2019-11-10 14:18:56 -0800

    Make file and .txt to .in

---
+++ make.bat
@@ -0,0 +1,8 @@
+javac *.java
+java Sudoku nakedSingle.txt > nakedSingle.out
+java Sudoku hiddenSingle.txt > hiddenSingle.out
+REM java Sudoku nakedDouble.txt > nakedDouble.out
+java Sudoku easy.txt > easy.out
+java Sudoku tough.txt > tough.out
+java Sudoku diabolical.txt > diabolical.out
+java Sudoku extreme7.txt > extreme7.out
